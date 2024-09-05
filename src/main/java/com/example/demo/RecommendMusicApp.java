d6af8a691657b7d5a01001ca66267aef57e2762e
itsmebgj <bgj3088@gmail.com>
2024-09-05 18:29:01 +0900

    modify DemoApplication to RecommendMusicApp

--- src/main/java/com/example/demo/RecommendMusicApp.java
+++ src/main/java/com/example/demo/RecommendMusicApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RecommendMusicApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RecommendMusicApp.class, args);
 	}
 
 }
